d73a24d723e36767ef51799b674d45788da38358
Paul Nation <nonhermitian@gmail.com>
2020-11-09 04:24:35 -0500

    Allow for autocomplete of backends from provider.

--- qiskit/providers/aqt/aqt_provider.py
+++ qiskit/providers/aqt/aqt_provider.py
@@ -19,7 +19,7 @@ from qiskit.providers import BaseProvider
 from .aqt_backend import AQTSimulator, AQTSimulatorNoise1, AQTDevice
 
 
-class AQTProvider(BaseProvider):
+class AQTProvider():
     """Provider for backends from Alpine Quantum Technologies (AQT)."""
 
     def __init__(self, access_token):
@@ -28,9 +28,9 @@ class AQTProvider(BaseProvider):
         self.access_token = access_token
         self.name = 'aqt_provider'
         # Populate the list of AQT backends
-        self._backends = [AQTSimulator(provider=self),
-                          AQTSimulatorNoise1(provider=self),
-                          AQTDevice(provider=self)]
+        self.backends = BackendService([AQTSimulator(provider=self),
+                                        AQTSimulatorNoise1(provider=self),
+                                        AQTDevice(provider=self)])
 
     def __str__(self):
         return "<AQTProvider(name={})>".format(self.name)
@@ -38,8 +38,31 @@ class AQTProvider(BaseProvider):
     def __repr__(self):
         return self.__str__()
 
-    def backends(self, name=None, filters=None, **kwargs):
+
+class BackendService():
+    """A service class that allows for autocompletion
+    of backends from provider.
+    """
+
+    def __init__(self, backends):
+        """Initialize service
+
+        Parameters:
+            backends (list): List of backend instances.
+        """
+        self._backends = backends
+        for backend in backends:
+            setattr(self, backend.name(), backend)
+
+    def __call__(self, name=None, filters=None, **kwargs):
         """A listing of all backends from this provider.
+
+        Parameters:
+            name (str): The name of a given backend.
+            filters (callable): A filter function.
+
+        Returns:
+            list: A list of backends, if any.
         """
         # pylint: disable=arguments-differ
         backends = self._backends
