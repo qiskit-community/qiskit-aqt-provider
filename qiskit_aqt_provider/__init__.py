d7bfafec7981a82cdca298980194cbff2bbe6230
Etienne Wodey <44871469+airwoodix@users.noreply.github.com>
2023-03-28 16:44:16 +0200

    Autoload environment from file (alpine-quantum-technologies/qiskit-aqt-provider-rc#42)
    * init: auto-load environment from .env file
    
    * deps/dev: add ipykernel (needed for docs)
    
    * docs: remove references to Arnica
    
    ---------
    
    Co-authored-by: Wilfried Huss <84843123+wilfried-huss@users.noreply.github.com>

--- qiskit_aqt_provider/__init__.py
+++ qiskit_aqt_provider/__init__.py
@@ -10,6 +10,6 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-from .aqt_provider import AQTProvider
+from qiskit_aqt_provider.aqt_provider import AQTProvider
 
 __all__ = ["AQTProvider"]
