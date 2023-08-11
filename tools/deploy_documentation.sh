e4b5fc1e4ef9da1a7dc68ef4a8a4869446d9620a
Matthew Treinish <mtreinish@kortar.org>
2023-08-11 07:03:33 -0400

    Update tools/deploy_documentation.sh

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -30,5 +30,4 @@ pwd
 # Push to qiskit.org/ecosystem
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 echo "Pushing built docs to qiskit.org/ecosystem"
-rclone sync --progress ./docs/_build IBMCOS:qiskit-org-web-resources/documentation/partners/aqt
 rclone sync --progress ./docs/_build IBMCOS:qiskit-org-web-resources/ecosystem/aqt-provider
