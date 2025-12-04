0c3c45687d3c5192d243b8e0bf14841a59c01a0a
florian.girtler <florian.girtler@aqt.eu>
2025-12-04 18:26:50 +0100

    first steps towards polling separation

--- qiskit_aqt_provider/api_client/portal_client.py
+++ qiskit_aqt_provider/api_client/portal_client.py
@@ -51,7 +51,9 @@ class PortalClient:
             headers["Authorization"] = f"Bearer {token}"
 
         self._http_client = httpx.Client(
-            base_url=self.portal_url.join("/api/v1"),
+            base_url=self.portal_url.join(
+                "/api/v1"
+            ),  # TODO: align with the URL used in ArnicaConfig
             headers=headers,
             timeout=timeout,
             follow_redirects=True,
